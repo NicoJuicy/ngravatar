bee67c1ccac0ee70e3c09cefb357a5950a3e7056
Nico Sap <nico@sapico.me>
2020-05-14 14:06:04 +0200

    Needed it quickly, migrated a weird bug that OData requires v7 instead of v5 dll version when using NGravatar ( don't know how this can be since there is no dependency)
    I didn't migrate the tests, but the Sample works.
    The namespace changed from NGravatar.Html to NGravatar.Web
    
    The project without the Web is now just NGravatar instead of NGravatar.Core

---
+++ NGravatar.Web/Utils/HtmlAttributeTypeFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Routing;
+
+namespace NGravatar.Web.Utils
+{
+    internal class HtmlAttributeTypeFilter
+    {
+
+        public IDictionary<string, object> FilterToDictionary(object htmlAttributes)
+        {
+            if (null == htmlAttributes) return null;
+
+            var collection = htmlAttributes as IEnumerable<KeyValuePair<string, object>>;
+            return (collection ?? new RouteValueDictionary(htmlAttributes))
+                .ToDictionary(
+                    pair => pair.Key,
+                    pair => pair.Value
+                );
+        }
+    }
+}
--- NGravatar/Properties/AssemblyInfo.cs
+++ NGravatar/Properties/AssemblyInfo.cs
@@ -1,18 +1,36 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("NGravatar")]
-[assembly: AssemblyDescription("NGravatar")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ken Yourek")]
+[assembly: AssemblyCompany("HP Inc.")]
 [assembly: AssemblyProduct("NGravatar")]
-[assembly: AssemblyCopyright("Copyright � Ken Yourek 2013")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.1")]
-[assembly: AssemblyFileVersion("1.0.1")]
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
-[assembly: InternalsVisibleTo("NGravatar.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce18895b-070a-46fd-97f6-14d337f401ad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
